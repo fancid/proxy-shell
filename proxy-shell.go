3769ac5cb607fb887c06854b3c1ae2c02930fffb
fancid <nate.m.reynolds@gmail.com>
2026-04-12 08:59:05 -0400

    Test passed

--- proxy-shell.go
+++ proxy-shell.go
@@ -24,21 +24,40 @@ func fetch(targetURL string) ([]string, error) {
 func main() {
 	// TODO: Add Proxy Grabber
 	// TODO: Test Proxy Grabber
-	apiURL := "https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all"
-
-	fmt.Println("Fetching proxies...")
-	proxies, err := fetch(apiURL)
-	if err != nil {
-		fmt.Printf("Error: %v\n", err)
-		return
+	apiURL := []string{
+		"https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all",
+		"https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/http.txt",
+		"https://raw.githubusercontent.com/shiftytr/proxy-list/master/proxy.txt",
+		"https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/http.txt",
 	}
 
-	// Print the list
-	for i, proxy := range proxies {
-		fmt.Printf("[%d] %s\n", i+1, proxy)
+	proxyMap := make(map[string]bool)
+
+	for _, url := range apiURL {
+		fmt.Printf("Fetching from: %s\n", url)
+
+		results, err := fetch(url)
+		if err != nil {
+			fmt.Printf("  Skipping source due to error: %v\n", err)
+			continue
+		}
+
+		// Add each found proxy to our map
+		for _, p := range results {
+			if p != "" { // Ensure we don't add empty strings
+				proxyMap[p] = true
+			}
+		}
+		fmt.Printf("  Successfully added %d proxies.\n", len(results))
 	}
 
-	fmt.Printf("\nTotal proxies found: %d\n", len(proxies))
+	// Print the final grouped list
+	fmt.Println("\n--- Proxy List ---")
+	for proxy := range proxyMap {
+		fmt.Println(proxy)
+	}
+
+	fmt.Printf("\nProxies: %d\n", len(proxyMap))
 	// TODO: Add Proxy Checker
 	// TODO: Test Proxy Checker
 	// TODO: Add Proxy Router

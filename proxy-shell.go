447fc428eac117e9e74dd2844bb320e9e7f7d1f9
fancid <nate.m.reynolds@gmail.com>
2026-04-12 09:40:02 -0400

    refactor + small optimization

--- proxy-shell.go
+++ proxy-shell.go
@@ -6,6 +6,42 @@ import (
 	"net/http"
 )
 
+func fetchCycle() {
+	apiURL := []string{
+		"https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/http.txt",
+		"https://raw.githubusercontent.com/shiftytr/proxy-list/master/proxy.txt",
+		"https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/http.txt",
+	}
+
+	proxyMap := make(map[string]bool)
+
+	for _, url := range apiURL {
+		var i int = 0
+		fmt.Printf("Fetching from: %s\n", url)
+
+		results, err := fetch(url)
+		if err != nil {
+			fmt.Printf("  Skipping source due to error: %v\n", err)
+			continue
+		}
+
+		// Add each found proxy to our map
+		for _, p := range results {
+			if p != "" { // Ensure we don't add empty strings
+				proxyMap[p] = true
+				i++
+			}
+		}
+		if i == 0 {
+			fmt.Printf("  No Proxies Found in %s", url)
+		} else {
+			fmt.Printf("  Successfully added %d proxies.\n", i)
+		}
+	}
+
+	fmt.Printf("\nTotal Proxies Fetched: %d\n", len(proxyMap))
+}
+
 func fetch(targetURL string) ([]string, error) {
 	resp, err := http.Get(targetURL)
 	if err != nil {
@@ -22,42 +58,7 @@ func fetch(targetURL string) ([]string, error) {
 }
 
 func main() {
-	// TODO: Add Proxy Grabber
-	// TODO: Test Proxy Grabber
-	apiURL := []string{
-		"https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all",
-		"https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/http.txt",
-		"https://raw.githubusercontent.com/shiftytr/proxy-list/master/proxy.txt",
-		"https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/http.txt",
-	}
-
-	proxyMap := make(map[string]bool)
-
-	for _, url := range apiURL {
-		fmt.Printf("Fetching from: %s\n", url)
-
-		results, err := fetch(url)
-		if err != nil {
-			fmt.Printf("  Skipping source due to error: %v\n", err)
-			continue
-		}
-
-		// Add each found proxy to our map
-		for _, p := range results {
-			if p != "" { // Ensure we don't add empty strings
-				proxyMap[p] = true
-			}
-		}
-		fmt.Printf("  Successfully added %d proxies.\n", len(results))
-	}
-
-	// Print the final grouped list
-	fmt.Println("\n--- Proxy List ---")
-	for proxy := range proxyMap {
-		fmt.Println(proxy)
-	}
-
-	fmt.Printf("\nProxies: %d\n", len(proxyMap))
+	fetchCycle()
 	// TODO: Add Proxy Checker
 	// TODO: Test Proxy Checker
 	// TODO: Add Proxy Router
